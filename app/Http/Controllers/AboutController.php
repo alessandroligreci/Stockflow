70760bc8864ed18b8c354319aaabaab0921890db
alessandroligreci <alessandroligreci@ied.edu>
2018-03-13 16:02:12 +0100

    ancora lezione13
    about controller
    contact form request

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Http\Requests\ContactFormRequest
 
-class AboutController extends Controller {
+class AboutController extends Controller
+{
     public function create()
     {
         return view('about.contact');
     }
     public function store()
     {
-        
+        return Redirect::route('contact')->with('message', 'Grazie per averci contattato');
     }
 }
