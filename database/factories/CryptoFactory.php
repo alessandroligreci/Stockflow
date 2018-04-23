f60a056c85fd6fac0195549b40b697c4bce7eeaa
alessandroligreci <alessandroligreci@ied.edu>
2018-04-23 13:49:44 +0200

    randomCrypto
    nel database non verrà salvato un nome a caso ma il nome di una
    cryptovaluta tra le prime 100

--- database/factories/CryptoFactory.php
+++ database/factories/CryptoFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(StockFlowSite\Crypto::class, function (Faker $faker) {
     return [
-        'name'=>$faker->word,
+        'name'=>$faker->randomElement(['Bitcoin','Ether','Ripple','Bitcoin Cash', 'Litecoin', 'EOS', 'Cardano', 'Stellar', 'IOTA', 'NEO', 'Monero', 'NEM', 'Dash', 'TRON', 'Tether', 'VeChain', 'Ether Classic', 'Verge', 'OmiseGo', 'Qtum', 'Binance Coin', 'Lisk', 'ICON', 'Ontology', 'Populous', 'Bitcoin Gold', 'Zcash', 'Bytom', 'Nano', 'Bytecoin', 'Steem', 'Siacoin', 'Wanchain', 'Bitshares', 'Dogecoin', 'Bitcoin Private', 'Stratis', 'DigixDAO', 'Waves', 'Ziliqa', 'Maker', 'Bitcoin Diamond', 'Decred', 'Status', 'Aeternity', 'RChain', '0x', 'Aion', 'Loopring', 'Komodo', 'Golem', 'Ardor', 'Augur', 'Digibyte', 'IOSToken', 'Waltonchain', 'KuCoin Shares', 'HShare', 'Basic Attention Token', 'Ark', 'Substratum', 'PIVX', 'aelf', 'Cryptonex', 'Centrality', 'Factom', 'Dragonchain', 'Mithril', 'QASH', 'Veritaseum', 'MonaCoin', 'SysCoin', 'Gas', 'FunFair', 'Ethos', 'Nxt', 'Elastos', 'Kyber Network', 'Nucleus Vision', 'Nebulas', 'Byteball Bytes', 'GXChain', 'WAX', 'ReddCoin', 'Dentacoin', 'Ethos BQX', 'Revain', 'SALT', 'Power Ledger', 'MaidSafeCoin', 'Mixin', 'ChainLink', 'ZCoin', 'Electroneum', 'Storm', 'Bancor', 'Enigma', 'Request Network', 'Kin', 'TenX', 'Neblio']),
         'quantity'=>$faker->randomFloat($nbMaxDecimals = 8, $min = 0, $max = 999999999.99999999),
         'value'=>$faker->randomFloat($nbMaxDecimals = 8, $min = 0, $max = 999999999.99999999),
         'user_id'=>factory('StockFlowSite\User')->create()->id,
